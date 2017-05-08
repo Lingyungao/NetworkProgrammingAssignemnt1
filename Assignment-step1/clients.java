452660edee77b7298c18773ba86e1f20184a02c2
Lingyungao <s3468120@student.rmit.edu.au>
2017-05-09 00:55:55 +1000

    try change

--- Assignment-step1/clients.java
+++ Assignment-step1/clients.java
@@ -1,4 +1,53 @@
-
-public class clients {
-
-}
+import java.io.BufferedReader;  
+import java.io.DataInputStream;  
+import java.io.DataOutputStream;  
+import java.io.InputStreamReader;  
+import java.net.Socket;  
+  
+/** 
+ * 创建socket对象,指定服务器的ip地址和服务器监听的端口号 
+ * 客户端在new的时候,就发出了连接请求,服务器端就会进行处理 
+ * 客户端在while循环中执行操作: 
+ * 听，说，听，说... 
+ * 
+ */  
+public class clients {  
+    private final static String host = "localhost";  
+    private final static int port = 8888;  
+    public static void main(String[] args) {  
+        try {  
+            Socket s = new Socket(host,port);  
+            System.out.println("客户端启动,发送请求...");  
+            //打开并封装输出流  
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());  
+            //打开并封装输入流  
+            DataInputStream dis = new DataInputStream(s.getInputStream());  
+            //读取并封装键盘输入流  
+            BufferedReader br = new BufferedReader(  
+                    new InputStreamReader(System.in));  
+            String info;  
+            while(true){  
+                //客户端先读取键盘输入信息  
+                info = br.readLine();  
+                //写入到服务器  
+                dos.writeUTF(info);  
+                //如果客户端自己说bye,即结束对话  
+                if(info.equals("bye")){  
+                    break;  
+                }  
+                //接收服务器端信息  
+                info = dis.readUTF();  
+                System.out.println("服务器说: "+ info);  
+                if(info.equals("bye")){  
+                    break;  
+                }  
+            }  
+            dis.close();  
+            dos.close();  
+            s.close();  
+  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+    }  
+}  
--- Assignment-step1/server.java
+++ Assignment-step1/server.java
@@ -1,4 +1,64 @@
-
-public class server {
-
-}
+     
+    import java.io.BufferedReader;  
+    import java.io.DataInputStream;  
+    import java.io.DataOutputStream;  
+    import java.io.IOException;  
+    import java.io.InputStreamReader;  
+    import java.net.ServerSocket;  
+    import java.net.Socket;  
+      
+    /**  
+     * 服务器的工作就是在指定的端口上监听 
+     * 建立连接 
+     * 打开输出流 
+     * 封装输出流 
+     * 向客户端发送数据 
+     * 关闭打开的输出流 
+     * 关闭打开的Socket对象 
+     * 服务器端的程序,在while循环中所执行的动作 
+     * 听，说，听，说... 
+     * @author Administrator 
+     * 
+     */  
+    public class server {  
+        private static final int port = 8888;  
+        public static void main(String[] args) {  
+            try {  
+                ServerSocket ss = new ServerSocket(port);  
+                //socket对象调用accept方法,等待请求的连接  
+                System.out.println("服务器已启动,等待请求...");  
+                Socket s = ss.accept();  
+                //打开并封装输出流  
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());  
+                //打开并封装输入流  
+                DataInputStream dis = new DataInputStream(s.getInputStream());  
+                //封装并读取键盘上的输入流  
+                BufferedReader br = new BufferedReader  
+                        (new InputStreamReader(System.in));  
+                String info;  
+                while(true){  
+                    //接收客户端发过来的信息  
+                    info = dis.readUTF();  
+                    System.out.println("server:"+info);  
+                    //如果发现对方说bye,则结束会话  
+                    if(info.equals("bye")){  
+                        break;  
+                    }  
+                    //读取键盘上的输入流,写入客户端  
+                    info = br.readLine()+"+1s";
+                    dos.writeUTF(info);  
+                    //如果服务器自己说bye,也结束会话  
+                    if(info.equals("bye")){  
+                        break;  
+                    }  
+                }  
+                dis.close();  
+                dos.close();  
+                s.close();  
+                ss.close();  
+            } catch (IOException e) {  
+                e.printStackTrace();  
+                System.out.println("网络连接异常,退出程序...");  
+            }  
+        }  
+    }  
