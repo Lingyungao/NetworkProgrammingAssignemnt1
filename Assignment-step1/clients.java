407d59d07913f9e73d59fbbd3763867da955e354
Lingyungao <s3468120@student.rmit.edu.au>
2017-05-10 07:28:40 +1000

    single play all done

--- Assignment-step1/clients.java
+++ Assignment-step1/clients.java
@@ -1,58 +1,68 @@
-import java.io.BufferedReader;  
-import java.io.DataInputStream;  
-import java.io.DataOutputStream;  
-import java.io.InputStreamReader;  
-import java.net.Socket;  
-  
-/** 
- * 创建socket对象,指定服务器的ip地址和服务器监听的端口号 
- * 客户端在new的时候,就发出了连接请求,服务器端就会进行处理 
- * 客户端在while循环中执行操作: 
- * 听，说，听，说... 
- * 
- */  
-public class clients {  
-    private final static String host = "localhost";  
-    private final static int port = 8888;  
-    public static void main(String[] args) {  
-        try {  
-            Socket s = new Socket(host,port);  
-            System.out.println("客户端启动,发送请求...");  
-            //打开并封装输出流  
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream());  
-            //打开并封装输入流  
-            DataInputStream dis = new DataInputStream(s.getInputStream());  
-            //读取并封装键盘输入流  
-            BufferedReader br = new BufferedReader(  
-                    new InputStreamReader(System.in));  
-            String info;  
-            while(true){  
-                //客户端先读取键盘输入信息  
-                info = br.readLine();
-                if(info.equals(0) || info.equals(1)){
-                	
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
-                //写入到服务器  
-                dos.writeUTF(info);  
-                //如果客户端自己说bye,即结束对话  
-                if(info.equals("bye")){  
-                    break;  
-                }  
-                //接收服务器端信息  
-                info = dis.readUTF();  
-                System.out.println("服务器说: "+ info); } 
-                if(info.equals("bye")){  
-                    break;  
-                }
-                else
-                	break;
-            }  
-            dis.close();  
-            dos.close();  
-            s.close();  
-  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-    }  
-}  
+public class clients {
+      private final static String host = "localhost";
+      private final static int port = 8888;
+
+      public static void main(String[] args) {
+           try {
+                 Socket s = new Socket(host, port);
+                 System.out.println("clients start.");
+                 // output steam
+                 DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                 // input steam
+                 DataInputStream dis = new DataInputStream(s.getInputStream());
+                 // read from keyboard
+                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                 String info;
+                 int resultNumber;
+                 while (true) {
+                     System.out.println("Please enter a number between 0-2");                      
+                     resultNumber = Integer.parseInt(br.readLine());
+                     //System.out.println("server number is:"+resultNumber);
+                     while (2<resultNumber||resultNumber<0){
+                            // read from keyboard
+                            System.out.println("Wrong input,Please enter a number between 0-2");                      
+                            //info = br.readLine();
+                            resultNumber = Integer.parseInt(br.readLine());
+                          //  System.out.println("server number is:"+resultNumber);
+                            // if user say bye,end
+                            // get server information
+                      }
+                      // send to server first time
+                      dos.writeInt(resultNumber);                      
+                     // System.out.println("send" + info + "out,plase say the guess number");
+                 break;
+                 }
+                 while (true) {
+                     System.out.println("Please guess sum,sum should between 0-4");                      
+                     resultNumber = Integer.parseInt(br.readLine());
+                     System.out.println("guess number is:"+resultNumber);
+                     while (4<resultNumber||resultNumber<0){
+                            // read from keyboard
+                            System.out.println("Wrong input,Please enter a number between 0-4");                      
+                            //info = br.readLine();
+                            resultNumber = Integer.parseInt(br.readLine());
+                         //   System.out.println("server number is:"+resultNumber);
+                            // if user say bye,end
+                            // get server information
+                      }
+                      // send to server first time
+                      dos.writeInt(resultNumber);
+                      info = dis.readUTF();
+                      System.out.println(info);
+                      break;
+                      }
+                 dis.close();
+                 dos.close();
+                 s.close();
+
+           } catch (Exception e) {
+                 e.printStackTrace();
+           }
+      }
+}
--- Assignment-step1/server.java
+++ Assignment-step1/server.java
@@ -1,64 +1,68 @@
-     
-    import java.io.BufferedReader;  
-    import java.io.DataInputStream;  
-    import java.io.DataOutputStream;  
-    import java.io.IOException;  
-    import java.io.InputStreamReader;  
-    import java.net.ServerSocket;  
-    import java.net.Socket;  
-      
-    /**  
-     * 服务器的工作就是在指定的端口上监听 
-     * 建立连接 
-     * 打开输出流 
-     * 封装输出流 
-     * 向客户端发送数据 
-     * 关闭打开的输出流 
-     * 关闭打开的Socket对象 
-     * 服务器端的程序,在while循环中所执行的动作 
-     * 听，说，听，说... 
-     * @author Administrator 
-     * 
-     */  
-    public class server {  
-        private static final int port = 8888;  
-        public static void main(String[] args) {  
-            try {  
-                ServerSocket ss = new ServerSocket(port);  
-                //socket对象调用accept方法,等待请求的连接  
-                System.out.println("服务器已启动,等待请求...");  
-                Socket s = ss.accept();  
-                //打开并封装输出流  
-                DataOutputStream dos = new DataOutputStream(s.getOutputStream());  
-                //打开并封装输入流  
-                DataInputStream dis = new DataInputStream(s.getInputStream());  
-                //封装并读取键盘上的输入流  
-                BufferedReader br = new BufferedReader  
-                        (new InputStreamReader(System.in));  
-                String info;  
-                while(true){  
-                    //接收客户端发过来的信息  
-                    info = dis.readUTF();  
-                    System.out.println("server:"+info);  
-                    //如果发现对方说bye,则结束会话  
-                    if(info.equals("bye")){  
-                        break;  
-                    }  
-                    //读取键盘上的输入流,写入客户端  
-                    info = br.readLine()+"+1s";
-                    dos.writeUTF("苟利国家生死以"+info);  
-                    //如果服务器自己说bye,也结束会话  
-                    if(info.equals("bye")){  
-                        break;  
-                    }  
-                }  
-                dis.close();  
-                dos.close();  
-                s.close();  
-                ss.close();  
-            } catch (IOException e) {  
-                e.printStackTrace();  
-                System.out.println("网络连接异常,退出程序...");  
-            }  
-        }  
-    }  
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+public class server {
+private static final int port = 8888;
+
+public static void main(String[] args) {
+try {
+ServerSocket ss = new ServerSocket(port);
+// connect
+System.out.println("server start, waiting");
+Socket s = ss.accept();
+// output steam
+DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+// input steam
+DataInputStream dis = new DataInputStream(s.getInputStream());
+// read from keyboard
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+String info;
+int max=2;
+int min=0;
+Random random = new Random();
+int randomNumber = random.nextInt(max)%(max-min+1) + min;
+int resultNumber;
+int sumNumber;
+while (true) {
+//get user input
+resultNumber = dis.readInt();
+//info = dis.readUTF();
+System.out.println("server:" + resultNumber);
+// if user say "bye",stop connect
+// read from keyboard and send to user
+sumNumber = resultNumber + randomNumber;
+System.out.println("server number is:"+resultNumber);
+System.out.println("random Number is:"+randomNumber);
+System.out.println("sum number is:"+sumNumber);
+info = "server:please enter guess number";
+break;
+}
+while (true) {
+	//get user input
+	resultNumber = dis.readInt();
+	//info = dis.readUTF();
+	System.out.println("server guess:" + resultNumber);
+	if((sumNumber-resultNumber)>0){
+		info = "you win";
+	}
+	else
+		info = "you fail";
+	dos.writeUTF(info);
+	break;
+	}
+dis.close();
+dos.close();
+s.close();
+ss.close();
+} catch (IOException e) {
+e.printStackTrace();
+System.out.println("sth error");
+		}
+	}
+}
